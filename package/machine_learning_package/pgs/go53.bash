6d8ce377d1f84f75c9f64e28a7cd45d1f05664c4
Archons1999 <ck1031052@gl.ck.tp.edu.tw>
2021-09-27 11:06:36 +0800

    update

--- package/machine_learning_package/pgs/go53.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -f
-
-./nexuni classifier train cfg/nexuni53.data cfg/nexuni53.cfg backup53/init.weights
--- package/machine_learning_package/pgs/va53.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -f
-
-./nexuni classifier valid cfg/nexuni53.data cfg/nexuni53_test.cfg backup53/darknet53_$1.weights
